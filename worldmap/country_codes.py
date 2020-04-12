c96ecd94f9c8b294f187094d6d318ba3e9a68252
Steffen Köhler <steffen.koehler@jat-gmbh.de>
2020-04-12 10:38:05 +0200

    add some notebooks

---
+++ worldmap/country_codes.py
@@ -0,0 +1,34 @@
+from pygal.maps.world import COUNTRIES
+
+additional_name = {
+    "Russia": "ru",
+    "US": "us",
+    "Bolivia": "bo",
+    "Brunei": "bn", 
+    "Czechia": "cz",
+    "Iran": "ir",
+    "Korea, South": "kr",
+    "Laos": "la",
+    "Libya":  "ly",
+    "Moldova": "md",
+    "North Macedonia": "mk",
+    "Qatar": "gq",
+    "South Sudan": "sd",
+    "Syria": 'sy',
+    "Taiwan": "tw",
+    "Venezuela": "ve",
+    "Vietnam": "vn"
+}
+
+def get_country_code(country_name):
+    return_code = None
+    for code,name in COUNTRIES.items():
+        if name == country_name:
+            return_code = code
+            break
+    
+    if not return_code:
+        return_code = additional_name[country_name] if country_name in additional_name else None
+        
+    return return_code
+
